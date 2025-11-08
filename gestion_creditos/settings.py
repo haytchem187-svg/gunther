83536bc5644d941ff1e489c8fe89244f3fa0acc0
kkrakker83 <150302941+kkrakker83@users.noreply.github.com>
2025-11-07 21:05:32 -0300

    Refactor settings for environment variables and CSRF
    Updated settings to use environment variables for SECRET_KEY and ALLOWED_HOSTS. Added CSRF_TRUSTED_ORIGINS for production.

--- gestion_creditos/settings.py
+++ gestion_creditos/settings.py
@@ -11,22 +11,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-26*bxygl4h(lnf+@d4n$e=gt11@h1+4w7(h73w5%96&kbsk86k"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-26*bxygl4h(lnf+@d4n$e=gt11@h1+4w7(h73w5%96&kbsk86k')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Cambia esta línea por defecto
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+# <-- AÑADE ESTO para solucionar el problema de CSRF -->
+CSRF_TRUSTED_ORIGINS = [
+    "https://peaceful-surprise-production-b854.up.railway.app"
+]
 
 # Application definition
 
@@ -70,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "gestion_creditos.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -80,7 +84,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -100,18 +103,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = "es-co"
-
 TIME_ZONE = "America/Bogota"
-
 USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
