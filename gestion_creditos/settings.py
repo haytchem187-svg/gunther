20387d8bf97a891f1197b9dbd8a4880e928962ab
kkrakker83 <150302941+kkrakker83@users.noreply.github.com>
2025-11-08 00:10:06 -0300

    Reorder ALLOWED_HOSTS for clarity

--- gestion_creditos/settings.py
+++ gestion_creditos/settings.py
@@ -30,9 +30,9 @@ DEBUG = True
 #ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 ALLOWED_HOSTS = [
-    "peaceful-surprise-production-b854.up.railway.app",
     "localhost",
-    "127.0.0.1"
+    "127.0.0.1",
+    "peaceful-surprise-production-b854.up.railway.app"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
